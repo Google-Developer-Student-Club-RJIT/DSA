613d570721e652f5965e2a4cb7b05f2e1648c818
RAHULANUP <98415824+RAHULANUP@users.noreply.github.com>
2022-10-25 13:06:26 +0530

    Update threenums.py

---
+++ threenums.py
@@ -0,0 +1,10 @@
+n1=int(input("ENTER NUMBER 1"))
+n2=int(input("ENTER NUMBER 2"))
+n3=int(input("ENTER NUMBER 3"))
+if(n1>n2 and n1>n3):
+    print(n1)
+elif(n2>n1 and n2>n3):
+    print(n2)
+else:
+    print(n3)
+    
