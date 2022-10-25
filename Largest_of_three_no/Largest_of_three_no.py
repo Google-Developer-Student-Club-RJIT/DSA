51c83f70b7229d904bc5b429785dfd6ba7fc3f1b
Khushal Neekhra <71013301+infinity-lightning@users.noreply.github.com>
2022-10-25 13:44:25 +0530

    Update Largest_of_three_no.py

--- Largest_of_three_no/Largest_of_three_no.py
+++ Largest_of_three_no/Largest_of_three_no.py
@@ -1,4 +1,4 @@
-def finglargest(a,b,c):
+def Largest_of_three_no(a,b,c):
     if(a>b)and(a>c):
         return a
     elif(b>a)and(b>c):
@@ -7,3 +7,5 @@ def finglargest(a,b,c):
         return c
     else:
         return ("numbers are equal")
+
+print(Largest_of_three_no(1,2,3))
