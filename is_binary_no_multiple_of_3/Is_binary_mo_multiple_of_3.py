1bb426d41261d2048ffea731b85ee4f6fd28d84f
abhijeet3447 <abhijeetkakde8@gmail.com>
2022-10-25 20:01:08 +0530

    Added python file

---
+++ is_binary_no_multiple_of_3/Is_binary_mo_multiple_of_3.py
@@ -0,0 +1,30 @@
+# problem link: https://practice.geeksforgeeks.org/problems/is-binary-number-multiple-of-30654/1?page=1&status[]=unsolved&category[]=Bit%20Magic&sortBy=submissions
+
+
+
+#User function Template for python3
+class Solution:
+    def isDivisible(self, s):
+        # code here
+        x=int(s,2)
+        t=0
+        x=str(x)
+        for i in x:
+            t+=int(i)
+        if t%3==0:
+            return 1
+        else:
+            return 0
+
+
+#{
+ # Driver Code Starts
+# Initial Template for Python 3
+if __name__ == '__main__':
+    T=int(input())
+    for i in range(T):
+        s = input()
+        ob = Solution()
+        ans = ob.isDivisible(s)
+        print(ans)
+# } Driver Code Ends
